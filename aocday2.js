f96729f0e0405f2978ab00a76545752e258ce78d
Josh <joshfenner93@icloud.com>
2019-12-13 15:03:03 +0000

    Fixed issue.

--- aocday2.js
+++ aocday2.js
@@ -6,15 +6,19 @@
 
 const fs = require("fs");
 
+const calculateFuelOnce = num => Math.floor((num / 3) - 2);
+
 function day2() {
 	fs.readFile("./intcode.txt", "utf-8", (err, data) => {
 		const intcode = data.toString();
 		const intcodeArray = intcode.split(",");
 		const answer = intcodeArray.reduce((total, num, index, arr) => {
+
 			if (num === "99") {
 				num = parseInt(num);
 				total += num;
 			}
+
 			return total;
 		}, null);
 		console.log(answer);
@@ -22,20 +26,3 @@ function day2() {
 }
 
 day2();
-
-function day1() {
-	fs.readFile("./day1.txt", "utf-8", (err, data) => {
-		const modules = data.toString();
-		const modulesArray = modules.split(",");
-		const answer = modulesArray.reduce((total, num, index, arr) => {
-				return arr;
-		}, null).find((item) => {
-			if (item > "0") {
-				return item;
-			}
-		});
-		console.log(answer);
-});
-}
-
-day1();
