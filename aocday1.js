6e38504aada02c8ac071a7125520fc8796402e10
Joshua Fenner <32834316+jjfenner93@users.noreply.github.com>
2020-03-31 13:47:07 +0100

    Updated.

--- aocday1.js
+++ aocday1.js
@@ -1,13 +1,15 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~Christmas 2019 - Advent of Code:~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//Day 1 Part 1 & 2:
-"use strict";
+// Day 1 Part 1 & 2:
+'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 const calculateFuelOnce = num => Math.floor((num / 3) - 2);
+
 /*
+// Old method below here without .reduce
 function storeAnswer(num) {
     let quotient = calculateFuelOnce(num);
 	let quotient2 = calculateFuelOnce(quotient);
@@ -18,7 +20,7 @@ function storeAnswer(num) {
 	let quotient7 = calculateFuelOnce(quotient6);
 	let quotient8 = calculateFuelOnce(quotient7);
 	let quotient9 = calculateFuelOnce(quotient8);
-    let arr = [num, quotient, quotient2, quotient3, quotient4, quotient5, 
+    let arr = [num, quotient, quotient2, quotient3, quotient4, quotient5,
 	quotient6, quotient7, quotient8, quotient9];
 	if (quotient9 <= 0) {
 		arr.pop();
@@ -43,22 +45,22 @@ function totalFuelRequirements() {
 
 totalFuelRequirements();
 */
+
 function day1() {
-	fs.readFile("./modules.txt", "utf-8", (err, data) => {
-		const modules = data.toString().split("\n");
+	fs.readFile('./modules.txt', 'utf-8', (err, data) => {
+		const modules = data.toString().split('\n');
 		const answer1 = modules.reduce((acc, currentValue, currentIndex, arr) => {
 		currentValue = parseInt(currentValue);
 		let total = acc + calculateFuelOnce(currentValue);
 		return total;
-	}, 0); //total can be another word for the function parameter 'accumulator' above.
+	}, 0); // total can be another word for the 'accumulator' above.
 	const answer2 = modules.reduce((acc, currentValue, currentIndex, arr) => {
 		let total = acc + calculateFuelOnce(currentValue);
-		return total; //Somehow access whatever has been accumulated and do calc.
+		return total; // Somehow access whatever has been accumulated and do calc.
 	}, 0);
 		console.log(`Total - part 1 is: ${answer1}`);
-		console.log(`Total - part 2 (refactored): ${answer2}`); //WIP (Incorrect answer)
-
-});
+		console.log(`Total - part 2 (refactored): ${answer2}`); // WIP (Incorrect answer here.)
+	});
 }
 
 day1();
