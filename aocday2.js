dcec6bc8f0abc58d2fb99f7db4be33ce9f67b393
Joshua Fenner <32834316+jjfenner93@users.noreply.github.com>
2020-08-10 15:54:34 +0100

    updated comments.

--- aocday2.js
+++ aocday2.js
@@ -2,20 +2,18 @@
 //~Christmas 2019 - Advent of Code:~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Day 2:
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 function day2() {
-	fs.readFile('./intcode.txt', 'utf-8', (err, data) => {
+	fs.readFile("./intcode.txt", "utf-8", (err, data) => {
 		const intcode = data.toString();
-		const intcodeArray = intcode.split(',');
+		const intcodeArray = intcode.split(","); // Turns it into an array.
 		const answer = intcodeArray.reduce((total, num, index, arr) => {
-
-			if (num === '99') {
+			if (num === "99") {
 				num = parseInt(num);
-				total += num;
-				// The num values that are 99 will be parsed to an integer for Maths calculations.
+				total += num; // The num values that are 99 will be parsed to an integer for Maths calculations.
 			}
 			return total;
 		}, null);
